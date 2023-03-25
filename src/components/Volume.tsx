0a88d14679d6b5b32c02359d25a2ccb03468fa68
eyezah <50851883+eyezahhhh@users.noreply.github.com>
2023-03-26 00:15:29 +1300

    initial commit

---
+++ src/components/Volume.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Volume.module.scss";
+
+export default function Volume() {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
