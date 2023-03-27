7fb9c1167c2fbea489f60ab1432da4e70d7a88a3
eyezah <50851883+eyezahhhh@users.noreply.github.com>
2023-03-27 21:59:37 +1300

    revert sidebar changes and add old theme

--- src/main.tsx
+++ src/main.tsx
@@ -31,46 +31,78 @@ const dark = createTheme({
       backgroundContrast: "#181825",
     },
     space: {},
-    fonts: {}
+    fonts: {
+      sans: "'Manrope', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif"
+    }
   }
 });
 
-const pink = createTheme({
-  type: "pink",
-  theme: {
-    colors: {
-      primaryLight: '#FEC5E5',
-      primaryLightHover: '#FEC5E5',
-      primaryLightActive: '#FEC5E5',
-      primaryLightContrast: '#FEC5E5',
-      primaryAlpha: '#cad3f5',
-      primary: '#FF1694',
-      primaryBorder: '#FD5DA8',
-      primaryBorderHover: '#FEC5E5',
-      primarySolidHover: '#FEC5E5',
-      primarySolidContrast: '$white',
-      primaryShadow: '#89b4fa',
+// const midnight = createTheme({
+//   type: "dark",
+//   theme: {
+//     colors: {
+//       primaryLight: '$green200',
+//       primaryLightHover: '$green300',
+//       primaryLightActive: '$green400',
+//       primaryLightContrast: '$green600',
+//       primaryAlpha: 'rgba(74, 222, 123, 0.2)',
+//       primary: '#4ADE7B',
+//       primaryBorder: '$green500',
+//       primaryBorderHover: '$green600',
+//       primarySolidHover: '$green700',
+//       primarySolidContrast: '$white',
+//       primaryShadow: '$green500',
+
+//       gradient: 'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+//       link: '#5E1DAD',
+
+//       sidebarUnselectedText: "$gray800",
+//       waitingBackground: "$gray300",
+//       backgroundSecondary: "#121414"
+//     },
+//     space: {},
+//     fonts: {
+//       sans: "'Manrope', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif"
+//     }
+//   }
+// });
+
+// const pink = createTheme({
+//   type: "dark",
+//   theme: {
+//     colors: {
+//       primaryLight: '#FEC5E5',
+//       primaryLightHover: '#FEC5E5',
+//       primaryLightActive: '#FEC5E5',
+//       primaryLightContrast: '#FEC5E5',
+//       primaryAlpha: '#cad3f5',
+//       primary: '#FF1694',
+//       primaryBorder: '#FD5DA8',
+//       primaryBorderHover: '#FEC5E5',
+//       primarySolidHover: '#FEC5E5',
+//       primarySolidContrast: '$white',
+//       primaryShadow: '#89b4fa',
       
 
 
-      gradient: 'linear-gradient(112deg, #FC46AA -25%, #FF1694 -10%, #FF1694 80%)',
-      link: '#b8c0e0',
-      hover: '#FC94AF',
+//       gradient: 'linear-gradient(112deg, #FC46AA -25%, #FF1694 -10%, #FF1694 80%)',
+//       link: '#b8c0e0',
+//       hover: '#FC94AF',
 
 
-      sidebarUnselectedText: "#FC94AF",
-      text: "#FC94AF",
-      waitingBackground: "#181825",
-      backgroundSecondary: "#181825",
-      background: "#11111b",
-      backgroundContrast: "#181825",
-    },
-    space: {},
-    fonts: {},
-    
-    
-  },
-});
+//       sidebarUnselectedText: "#FC94AF",
+//       text: "#FC94AF",
+//       waitingBackground: "#181825",
+//       backgroundSecondary: "#181825",
+//       background: "#11111b",
+//       backgroundContrast: "#181825",
+//     },
+//     space: {},
+//     fonts: {
+//       sans: "'Manrope', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif"
+//     }
+//   },
+// });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
