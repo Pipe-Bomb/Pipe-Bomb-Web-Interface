4ae58c917384f148403ec8e8431fc3fcb958c635
eyezah <50851883+eyezahhhh@users.noreply.github.com>
2023-04-05 14:31:11 +1200

    add m3u download to charts

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -4,13 +4,14 @@ import TrackList from "pipebomb.js/dist/collection/TrackList";
 import { useEffect, useState } from "react";
 import ChartIndex from "../logic/ChartIndex";
 import Loader from "../components/Loader";
-import { Button, Grid, Text } from "@nextui-org/react";
-import { shuffle } from "../logic/Utils";
+import { Button, Dropdown, Grid, Text } from "@nextui-org/react";
+import { convertTracklistToM3u, shuffle } from "../logic/Utils";
 import styles from "../styles/Chart.module.scss";
-import { MdPlayArrow, MdShuffle } from "react-icons/md";
+import { MdMoreHoriz, MdPlayArrow, MdShuffle } from "react-icons/md";
 import ListTrack from "../components/ListTrack";
 import NumberWrapper from "../components/NumberWrapper";
 import Track from "pipebomb.js/dist/music/Track";
+import PipeBombConnection from "../logic/PipeBombConnection";
 
 export default function Chart() {
     let paramID: any = useParams().chartID;
@@ -51,6 +52,16 @@ export default function Chart() {
         audioPlayer.nextTrack();
     }
 
+    function contextMenu(button: React.Key) {
+        switch (button) {
+            case "m3u":
+                if (trackList) {
+                    convertTracklistToM3u(PipeBombConnection.getInstance().getUrl(), trackList, false, true);
+                }
+                break;
+        }
+    }
+
     return (
         <>
             <Text h1>{chart.collectionName}</Text>
@@ -60,6 +71,19 @@ export default function Chart() {
                 </Grid>
                 <Grid>
                     <Button size="lg" auto onPress={shuffleChart} className={styles.roundButton} bordered><MdShuffle /></Button>
+                </Grid>
+                <Grid>
+                    <Dropdown>
+                        <Dropdown.Trigger>
+                            <Button light size="xl" className={styles.contextButton}>
+                                <MdMoreHoriz />
+                            </Button>
+                        </Dropdown.Trigger>
+                        <Dropdown.Menu onAction={contextMenu}>
+                            <Dropdown.Item key="m3u">Download as M3U</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    
                 </Grid>
             </Grid.Container>
             {trackList.map((track, index) => (
